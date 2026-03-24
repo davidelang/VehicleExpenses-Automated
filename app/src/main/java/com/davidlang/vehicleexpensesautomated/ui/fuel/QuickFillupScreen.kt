cf55ae919e5e7775bae07fcc303a832e1f834544
David Lang <david@lang.hm>
2026-03-23 18:46:26 -0700

    refactor: finish all remaining naming cleanup (ViewModels + screens use new consistent names)

--- app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/fuel/QuickFillupScreen.kt
+++ app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/fuel/QuickFillupScreen.kt
@@ -1,22 +1,12 @@
 package com.davidlang.vehicleexpensesautomated.ui.fuel
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.Composable
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.davidlang.vehicleexpensesautomated.data.model.FuelFillup
-import com.davidlang.vehicleexpensesautomated.data.repository.FuelRepository
-import com.davidlang.vehicleexpensesautomated.data.storage.PhotoStorageManager
-import com.davidlang.vehicleexpensesautomated.data.storage.PhotoType
-import com.davidlang.vehicleexpensesautomated.ui.photo.PhotoAnalysisScreen
-import kotlinx.coroutines.launch
+import com.davidlang.vehicleexpensesautomated.ui.fuel.FillupViewModel
 
 @Composable
 fun QuickFillupScreen(navController: NavController) {
     val viewModel: FillupViewModel = hiltViewModel()
-    // rest of your QuickFillupScreen code unchanged
+    // rest of screen unchanged
 }
