f80a59599c3ac1983bb7cc550dcfb6abb1756e91
David Lang <david@lang.hm>
2026-03-24 03:30:42 -0700

    fix(photo): add missing Toast import (final build fix)

--- app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/fuel/QuickFillupScreen.kt
+++ app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/fuel/QuickFillupScreen.kt
@@ -1,5 +1,6 @@
 package com.davidlang.vehicleexpensesautomated.ui.fuel
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
