8783bcd98e959327c67f5d8955cf3e186fb47698
David Lang <david@lang.hm>
2026-03-23 17:31:15 -0700

    fix(upgrade): bump KSP to 2.1.0-1.0.30 (fixes unexpected jvm signature V)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,16 @@
 plugins {
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
-    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
+    id("com.google.devtools.ksp") version "2.1.0-1.0.30" apply false
     id("com.android.application") version "9.0.0" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("com.google.dagger.hilt.android") version "2.59.2" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0" apply false
+}
+
+allprojects {
+    configurations.all {
+        resolutionStrategy {
+            force("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.9.0")
+        }
+    }
 }
