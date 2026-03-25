15aa2cc4df8428081bd651b6d14f4cca20d8a67d
David Lang <david@lang.hm>
2026-03-25 02:47:53 -0700

    fix(build): enable BuildConfig generation + dynamic git-describe versionName + correct import in AboutScreen.kt

--- app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/about/AboutScreen.kt
+++ app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/about/AboutScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import com.davidlang.vehicleexpensesautomated.BuildConfig
 
 @Composable
 fun AboutScreen() {
