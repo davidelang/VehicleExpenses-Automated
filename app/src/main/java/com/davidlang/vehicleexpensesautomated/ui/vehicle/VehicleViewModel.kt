cd9b772b6b0df9f443a28e458d2c4af03eb6a2db
David Lang <david@lang.hm>
2026-04-01 05:46:16 -0700

    Fix missing Log import in VehicleViewModel.kt
    - Added import android.util.Log
    - This resolves the unresolved reference error from last patch

--- app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/vehicle/VehicleViewModel.kt
+++ app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/vehicle/VehicleViewModel.kt
@@ -2,6 +2,7 @@ package com.davidlang.vehicleexpensesautomated.ui.vehicle
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.davidlang.vehicleexpensesautomated.data.model.Vehicle
