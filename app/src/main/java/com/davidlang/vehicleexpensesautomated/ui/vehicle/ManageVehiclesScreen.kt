4b96089a9c8fc2f8d72f1e773f6ef8d29c111689
David Lang <david@lang.hm>
2026-04-06 22:23:38 -0700

    Heavy CropDebug logging in onDrag and onDragEnd to diagnose why red preview never appears

--- app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/vehicle/ManageVehiclesScreen.kt
+++ app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/vehicle/ManageVehiclesScreen.kt
@@ -248,12 +248,13 @@ fun ManageVehiclesScreen(
                                     dragStart = offset
                                     dragOffset = Offset.Zero
                                     currentDragRect = null
-                                    Log.i("CropDebug", "Drag started at $offset")
+                                    Log.i("CropDebug", "onDragStart - offset=$offset")
                                 },
                                 onDrag = { change, dragAmount ->
                                     change.consume()
                                     dragOffset = Offset(dragOffset.x + dragAmount.x, dragOffset.y + dragAmount.y)
                                     val start = dragStart
+                                    Log.i("CropDebug", "onDrag called - dragAmount=$dragAmount, dragOffset=$dragOffset")
                                     if (start != null && imageSize.x > 0 && imageSize.y > 0 && originalImageSize.x > 0 && originalImageSize.y > 0) {
                                         val fitRect = calculateFitImageRect(imageSize.x, imageSize.y, originalImageSize.x, originalImageSize.y)
                                         val end = Offset(start.x + dragOffset.x, start.y + dragOffset.y)
@@ -267,6 +268,7 @@ fun ManageVehiclesScreen(
                                 },
                                 onDragEnd = {
                                     val start = dragStart
+                                    Log.i("CropDebug", "onDragEnd called - dragStart=$start")
                                     if (start != null && imageSize.x > 0 && imageSize.y > 0 && originalImageSize.x > 0 && originalImageSize.y > 0) {
                                         val fitRect = calculateFitImageRect(imageSize.x, imageSize.y, originalImageSize.x, originalImageSize.y)
                                         val end = Offset(start.x + dragOffset.x, start.y + dragOffset.y)
@@ -317,8 +319,8 @@ fun ManageVehiclesScreen(
                         otherTextCropRect?.let { rect ->
                             val left = fitRect.left + rect.left * fitRect.width
                             val top = fitRect.top + rect.top * fitRect.height
-                            val width = rect.width * fitRect.width
-                            val height = rect.height * fitRect.height
+                            the width = rect.width * fitRect.width
+                            the height = rect.height * fitRect.height
                             drawRect(Color.Green, Offset(left, top), androidx.compose.ui.geometry.Size(width, height), style = Stroke(4f))
                         }
                     }
