e9b22c17fe47efdab6c14cda71a5f3bdd04bff39
David Lang <david@lang.hm>
2026-04-17 06:52:44 -0700

    Architecture: Decouple OcrHarness and update GEMINI policy

--- app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/util/OdometerOcrUtils.kt
+++ app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/util/OdometerOcrUtils.kt
@@ -26,42 +26,6 @@ import java.io.FileOutputStream
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
-object OcrHarness {
-    suspend fun runDiscovery(bitmap: Bitmap, context: Context): Map<String, OcrResult> {
-        val paddleOcrTflite = PaddleOcrEngine(context, isConstrained = false)
-        val nativePaddle = NativePaddleEngine(context, isConstrained = false)
-        val enginesList = mutableListOf<OcrEngine>(MlKitEngine(), NativeTfliteEngine(context), paddleOcrTflite)
-        if (nativePaddle.isAvailable) enginesList.add(nativePaddle)
-
-        return enginesList.associate { engine ->
-            engine.name to engine.recognize(bitmap)
-        }
-    }
-
-    suspend fun runRefinement(bitmap: Bitmap, context: Context): Map<String, OcrResult> {
-        val paddleOcrTflite = PaddleOcrEngine(context, isConstrained = true)
-        val nativePaddle = NativePaddleEngine(context, isConstrained = true)
-        val enginesList = mutableListOf<OcrEngine>(TesseractEngine(), MlKitEngine(), NativeTfliteEngine(context), paddleOcrTflite)
-        if (nativePaddle.isAvailable) enginesList.add(nativePaddle)
-
-        return enginesList.associate { engine ->
-            engine.name to engine.recognize(bitmap)
-        }
-    }
-
-    fun getDiscoveryEngineNames(context: Context): List<String> {
-        val list = mutableListOf("ML Kit", "Native TFLite", "Paddle-TFLite")
-        if (NativePaddleEngine(context).isAvailable) list.add("Paddle-Lite")
-        return list
-    }
-
-    fun getRefinementEngineNames(context: Context): List<String> {
-        val list = mutableListOf("Tesseract", "ML Kit", "Native TFLite", "Paddle-TFLite (Odo)")
-        if (NativePaddleEngine(context, isConstrained = true).isAvailable) list.add("Paddle-Lite (Odo)")
-        return list
-    }
-}
-
 object OdometerOcrUtils {
     init {
         if (!OpenCVLoader.initLocal()) {
