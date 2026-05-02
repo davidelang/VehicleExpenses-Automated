cb9e01fc62335631787cb05da30e697c137e76b6
David Lang <david@lang.hm>
2026-05-02 00:36:16 -0700

    Fix: Added missing Paint import for shared reporting buffers

--- app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/util/NativePaddleEngine.kt
+++ app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/util/NativePaddleEngine.kt
@@ -3,6 +3,7 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
 import android.graphics.Rect
 import android.os.Build
 import android.util.Log
