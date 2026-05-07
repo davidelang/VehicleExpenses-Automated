c36e064dde980fb3768e6ae98ea0776a6b7952da
David Lang <david@lang.hm>
2026-05-06 18:55:46 -0700

    Fix compilation error: Remove duplicate Color import

--- app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/experiment/ExperimentAlignmentScreen.kt
+++ app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/experiment/ExperimentAlignmentScreen.kt
@@ -11,7 +11,6 @@ import android.graphics.Paint
 
 import android.graphics.RectF
 import android.graphics.Rect
-import android.graphics.Color
 import android.net.Uri
 import android.util.Base64
 import android.util.Log
