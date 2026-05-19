8566868634f25f338c75f1cef4a7cc3ffb4fe4ee
David Lang <david@lang.hm>
2026-05-19 06:25:48 -0700

    Correct native alignment matrix using WARP_INVERSE_MAP for geometric parity

--- app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/util/ImageAlignmentUtils.kt
+++ app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/util/ImageAlignmentUtils.kt
@@ -618,11 +618,11 @@ object ImageAlignmentUtils {
             val src = bufferSet.p.mat
             val dst = bufferSet.s.mat
             val warpMat = Mat(2, 3, CvType.CV_64F)
-            val invScale = 1.0 / finalScale.toDouble()
-            warpMat.put(0, 0, invScale, 0.0, -(finalTx * src.cols()).toDouble() * invScale)
-            warpMat.put(1, 0, 0.0, invScale, -(finalTy * src.rows()).toDouble() * invScale)
+            val s = finalScale.toDouble()
+            warpMat.put(0, 0, s, 0.0, (finalTx * src.cols()).toDouble())
+            warpMat.put(1, 0, 0.0, s, (finalTy * src.rows()).toDouble())
             
-            Imgproc.warpAffine(src, dst, warpMat, src.size(), Imgproc.INTER_CUBIC, Core.BORDER_CONSTANT, Scalar(0.0))
+            Imgproc.warpAffine(src, dst, warpMat, src.size(), Imgproc.INTER_CUBIC or Imgproc.WARP_INVERSE_MAP, Core.BORDER_CONSTANT, Scalar(0.0))
             bufferSet.flip()
             warpMat.release()
             
