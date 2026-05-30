04b27339e40fee4b6a17a2d9f1be18497528d6d2
David Lang <david@lang.hm>
2026-05-29 22:10:36 -0700

    Remove numeric confidence drop logic from shared processOcr

--- app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/util/NativePaddleEngine.kt
+++ app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/util/NativePaddleEngine.kt
@@ -346,10 +346,7 @@ class NativePaddleEngine(private val context: Context, private val variant: Stri
                 var maxIdx = 0; var maxVal = -1f; val searchLimit = dictSize
                 for (j in 0 until searchLimit) { val v = data[i * dictSize + j]; if (v > maxVal) { maxVal = v; maxIdx = j } }
                 if (maxIdx > 0 && maxIdx != lastIdx && maxIdx <= dictionary.size) {
-
-                    if (result.length < 4 || maxVal >= (0.60f * lastConf)) {
-                        result.append(dictionary[maxIdx - 1]); totalConf += maxVal; charCount++; lastConf = maxVal
-                    } else break
+                    result.append(dictionary[maxIdx - 1]); totalConf += maxVal; charCount++; lastConf = maxVal
                 }
                 lastIdx = maxIdx
             }
