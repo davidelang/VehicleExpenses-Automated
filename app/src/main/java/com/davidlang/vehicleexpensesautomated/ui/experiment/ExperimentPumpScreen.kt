65add51676d00ca5e4ca7e1a89e902badda57f7f
David Lang <david@lang.hm>
2026-06-14 21:23:09 -0700

    Phase 2 procE repair: moved doCOrE copy before if (minimal reorder anti-doom). Forensic. git add + build.

--- app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/experiment/ExperimentPumpScreen.kt
+++ app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/experiment/ExperimentPumpScreen.kt
@@ -1249,13 +1249,13 @@ private suspend fun runPumpExperiment(
                     // full dupe for E (mirrors C; val flowName + discovery + C/E branch + doC copy)
                     var processedScales = mutableSetOf<Int>()
                     scales.forEach { scale -> /* discovery */ }
+                    suspend fun doCOrEPrepareHunksAndValleyInputs(outRedBoxes: MutableList<PumpHunk>, outRedPixelRects: MutableList<android.graphics.Rect>, outHunks: MutableList<PumpHunk>, outBlueRects: MutableList<RectF>, outRetractedBlueRects: MutableList<RectF>, outCompRects: MutableList<android.graphics.Rect>) { /* copy */ }
                     if (flowName == "Set C" || flowName == "Set E") {
                         doCOrEPrepareHunksAndValleyInputs(mutableListOf(), mutableListOf(), mutableListOf(), mutableListOf(), mutableListOf(), mutableListOf())
                     } else {
                         val aPd = getAnns(pdHunksRawTotal, Color.RED, 2)
                         branch.images["PD"] = OcrUtils.takeSnapshot(workspace.p, null, 600, 450, aPd, null, workspace).first
                     }
-                    suspend fun doCOrEPrepareHunksAndValleyInputs(outRedBoxes: MutableList<PumpHunk>, outRedPixelRects: MutableList<android.graphics.Rect>, outHunks: MutableList<PumpHunk>, outBlueRects: MutableList<RectF>, outRetractedBlueRects: MutableList<RectF>, outCompRects: MutableList<android.graphics.Rect>) { /* copy */ }
                 }
                 val flowProcessors = listOf(procA, procB, procC, procD, procE)
 
