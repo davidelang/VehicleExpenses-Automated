2e1c68e514d22f207af216b5f0c8b8f56df0227c
David Lang <david@lang.hm>
2026-06-17 22:28:23 -0700

    Phase 19 fix: add RectF import

--- app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/util/NativePaddleEngine.kt
+++ app/src/main/java/com/davidlang/vehicleexpensesautomated/ui/util/NativePaddleEngine.kt
@@ -7,6 +7,7 @@ import android.graphics.ColorMatrix
 import android.graphics.ColorMatrixColorFilter
 import android.graphics.Paint
 import android.graphics.Rect
+import android.graphics.RectF
 import android.os.Build
 import android.util.Log
 import com.baidu.paddle.lite.MobileConfig
